7b2e07483a1de33e0935cfcbd2a7fafe496d7993
mahdi momeni <mahdimomeni6@gmail.com>
2025-03-26 21:56:27 +0330

    feat: add config and mongo

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {PostModule} from "./post/post.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
