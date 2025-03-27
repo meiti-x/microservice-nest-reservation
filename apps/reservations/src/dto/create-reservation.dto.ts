ffb223228fb5457e2a371bc62de9caa26223db06
mahdi momeni <mahdimomeni6@gmail.com>
2025-03-27 22:42:20 +0330

    feat: add class validation

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,7 +1,24 @@
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
   timestamp: Date;
+
+  @IsDate()
+  @Type(() => Date)
   startDate: Date;
+
+  @IsDate()
+  @Type(() => Date)
   endDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
   inVoiceId: string;
 }
